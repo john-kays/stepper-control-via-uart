055dbd34e28114ed0d06006d2db732927ec425f4
ThuanLe <thuanle.ute@gmail.com>
2021-01-05 08:16:26 +0700

    add: init ver

---
+++ stepper_motor/stepper_motor.ino
@@ -0,0 +1,56 @@
+#include <Stepper.h>
+
+//  Define serial input flags
+const int CW_X_FLAG = 1;  // Send a character '1' over serial to move horizontal-axis motor clockwise
+const int CCW_X_FLAG = 2; // Send a character '2' over serial to move horizontal-axis motor counterclockwise
+
+
+//  Define stepper motor pins
+//    Horizontal plane motor
+const int STEPPER_HP_1 = 8;
+const int STEPPER_HP_2 = 9;
+const int STEPPER_HP_3 = 10;
+const int STEPPER_HP_4 = 11;
+
+
+//  Define stepper motor parameters
+const int STEPPER_STEPS_PER_REV = 200; //  steps per revolution
+const int NUM_STEPS_PER_REQ = 10;
+const int STEPPER_SPEED = 35;
+
+//  Initialize the stepper motors
+//      CW = forward = positive step, CCW = backward = negative step
+//    Horizontal plane
+Stepper stepper_motor_hp(STEPPER_STEPS_PER_REV, STEPPER_HP_1, STEPPER_HP_2, STEPPER_HP_3, STEPPER_HP_4);
+
+
+//  Initialize some working variables
+char in_byte = ' ';
+int in_num;
+
+/*
+ * Expecting an ASCII character of an integer ('0' --> '9') as serial input, 
+ * returns that ASCII integer as an int.
+ */
+int get_serial_input_as_int() {
+  in_byte = Serial.read();
+
+  return int(in_byte - '0');  //  Input is ASCII character of a single digit integer. Convert to int.
+}
+
+
+void setup() {
+  Serial.begin(9600);
+
+  stepper_motor_hp.setSpeed(STEPPER_SPEED);
+
+}
+
+void loop() {
+  if(Serial.available()) {
+    in_num = get_serial_input_as_int();
+    
+    if(in_num == CCW_X_FLAG) { stepper_motor_hp.step(-NUM_STEPS_PER_REQ); }
+    else if (in_num == CW_X_FLAG) { stepper_motor_hp.step(NUM_STEPS_PER_REQ); }
+  }  
+}
