6c4aa43ba00dbf43608b4a35f370109f4d2d5eeb
ThuanLe <58209201+ThuanLeUte@users.noreply.github.com>
2021-01-09 22:59:25 +0700

    update: time

--- stepper_motor_new_driver/stepper_motor_new_driver.ino
+++ stepper_motor_new_driver/stepper_motor_new_driver.ino
@@ -3,7 +3,7 @@
  * @copyright  Copyright (C) 2020 ThuanLe. All rights reserved.
  * @license    This project is released under the ThuanLe License.
  * @version    1.0.0
- * @date       2020-08-26
+ * @date       2020-01-09
  * @author     Thuan Le
  * @brief      Support control the stepper by stepper driver
  * @note       None
