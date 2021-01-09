5151043ba2ab8edbec589d517dfa6f1884f82c08
ThuanLe <thuanle.ute@gmail.com>
2021-01-09 22:36:46 +0700

    update: adding new dirver

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # stepper-control-via-uart
+# II. HARDWARE CONNECTION
+## ------------------------------------
+## 1. PUL       => DIGITAL 8 (Arduino)
+## 2. DIR       => DIGITAL 9 (Arduino)
+## 3. ENA       => DIGITAL 10(Arduino)
+## ------------------------------------
+## 4. GND       = > GND  (12V Power)
+## 4. 12v       = > +Vdc (12v Power)
+## ------------------------------------
+## 4. A+        => Blue  (Stepper)
+## 4. A-        => Red   (Stepper)
+## 4. B+        => Green (Stepper)
+## 4. B-        => Black (Stepper)
+## ------------------------------------
+
 # II. COMMAND TO SET AND READ MOTOR STATUS
 
 ## 1. Setting command
@@ -7,3 +22,6 @@
  ### TR_180  : Turn counter clockwise 180 step (will be change to degree)
 
 ## 2. Reading command
+ ### MT      : Read motor multi turn angle
+ ### RP      : Read motor speed
+ ### RE      : Read motor encoder
--- stepper_motor_new_driver/stepper_motor_new_driver.ino
+++ stepper_motor_new_driver/stepper_motor_new_driver.ino
@@ -12,69 +12,177 @@
 
 /* Includes ----------------------------------------------------------- */
 #include <AccelStepper.h>
+#include "stdint.h"
 
 /* Private defines ---------------------------------------------------- */
-#define STEPPER_DIR_PIN      (18)
-#define STEPPER_STEP_PIN     (19)
-#define STEPPER_ENA_PIN      (20)
+#define STEPPER_STEP_PIN     (8)
+#define STEPPER_DIR_PIN      (9)
+#define STEPPER_ENA_PIN      (10)
+
+/*
+-----------------------------------------
+800     pulses  => 360 degrees
+800/360 pulses  => 1   degree
+-----------------------------------------
+800             => 1 revolution/second
+800/60          => 1 revolution/minute
+-----------------------------------------
+*/
+
+#define STEP_PER_DEGREE             (2.22)
+#define REVOLUTION_PER_MINUITE      (13.33)
+
+#define STEPPER_DEFAULT_SPEED_RPM   (60)
 
 /* Private enumerate/structure ---------------------------------------- */
 /* Private macros ----------------------------------------------------- */
 /* Public variables --------------------------------------------------- */
+/* Private constant --------------------------------------------------- */
+
+static const String SET_SPEED_CMD               = "SP";
+static const String CLOCKWISE_CMD               = "TL";
+static const String COUNTER_CLOCKWISE_CMD       = "TR";
+
+static const String READ_MULTI_TURN_ANGLE_CMD   = "MT";
+static const String READ_SPEED_CMD              = "RP";
+static const String READ_ENCODER_CMD            = "RE";
+static const String READ_TEMP_CMD               = "RT";
+
 /* Private variables -------------------------------------------------- */
+static String m_uart_data_receive = "";
+static String m_uart_cmd = "";
+static String m_uart_data = "";
+static boolean m_uart_string_complete = false;
+static float m_float_data_value = 0;
+
+static int16_t m_stepper_speed_rpm = STEPPER_DEFAULT_SPEED_RPM;
+static int16_t m_stepper_speed_ppm = REVOLUTION_PER_MINUITE * m_stepper_speed_rpm;
+
 AccelStepper stepper(AccelStepper::DRIVER, STEPPER_STEP_PIN, STEPPER_DIR_PIN);
 
 /* Private function prototypes ---------------------------------------- */
+static void uart_receive_and_execute(void);
+void m_stepper_set_position(int16_t degree);
+
 /* Function definitions ----------------------------------------------- */
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   stepper.setEnablePin(STEPPER_ENA_PIN);
   stepper.setMaxSpeed(100000);
-  stepper.setAcceleration(10000);
   stepper.setSpeed(5000);
-}
-
-void move_forward()
-{
-  stepper.moveTo(10000);
-  while (stepper.currentPosition() != 10000)
-  {
-    stepper.setSpeed(5000);
-    stepper.run();
-  }
-  stepper.stop();
-  Serial.println(stepper.currentPosition());
-}
-
-void move_backward()
-{
-  stepper.moveTo(0);
-  while (stepper.currentPosition() != 0)
-  {
-    stepper.setSpeed(-3000);
-    stepper.runSpeed();
-  }
-  stepper.stop();
-  Serial.println(stepper.currentPosition());
 }
 
 void loop()
 {
-  while (Serial.available())
+  uart_receive_and_execute();
+}
+
+/* Private function definitions --------------------------------------- */
+/**
+ * @brief       Uart receive data and execute
+ *
+ * @param[in]   None
+ *
+ * @attention   None
+ *
+ * @return      None
+ */
+static void uart_receive_and_execute(void)
+{
+  while (Serial.available()) // Receive data from computer
   {
-    char data = Serial.read();
-    if (char(data) == 'A')
+    char data = (char)Serial.read();
+    m_uart_data_receive += data;
+
+    if (data == '\n')
     {
-      move_forward();
+      m_uart_string_complete = true;
+    }
 
-      delay(1000);
+    if (m_uart_string_complete)
+    {
+      m_uart_string_complete = false;
+      Serial.println(m_uart_data_receive);
 
-      move_backward();
+      m_uart_cmd = m_uart_data_receive.substring(0, 2);
+      m_uart_data = m_uart_data_receive.substring(3);
+      m_float_data_value = m_uart_data.toFloat();
+      Serial.println(m_uart_cmd);
+      Serial.println(m_uart_data);
+      Serial.println(m_float_data_value);
+      m_uart_data_receive = "";
 
-      delay(1000);
+      if (CLOCKWISE_CMD == m_uart_cmd)
+      {
+        Serial.println("Set motor run clockwise");
+        m_stepper_set_position(m_float_data_value);
+      }
+      else if (COUNTER_CLOCKWISE_CMD == m_uart_cmd)
+      {
+        Serial.println("Set motor run counter clockwise");
+        m_stepper_set_position(-m_float_data_value);
+      }
+      else if (SET_SPEED_CMD == m_uart_cmd)
+      {
+        Serial.println("Set motor speed");
+        m_stepper_set_speed(m_float_data_value);
+      }
+      else if (READ_MULTI_TURN_ANGLE_CMD == m_uart_cmd)
+      {
+        Serial.print("Motor multi turn angle:");
+        Serial.println(stepper.currentPosition() / STEP_PER_DEGREE);
+      }
+      else if (READ_SPEED_CMD == m_uart_cmd)
+      {
+        Serial.print("Motor speed rpm: ");
+        Serial.println(m_stepper_speed_rpm);
+      }
+      else if (READ_ENCODER_CMD == m_uart_cmd)
+      {
+        int16_t encoder = 0;
+        encoder = (int)(stepper.currentPosition() / STEP_PER_DEGREE) % 360;
+        encoder = encoder < 0 ? (-encoder) : encoder;
+        Serial.print("Motor encoder: ");
+        Serial.println(encoder);
+      }
     }
   }
+}
+
+void m_stepper_set_position(int16_t degree)
+{
+  signed long step;
+  Serial.print("Step per degree: ");
+  Serial.println(STEP_PER_DEGREE);
+
+  step =  degree * STEP_PER_DEGREE;
+  Serial.print("Pulses have to run: ");
+  Serial.println(step);
+
+  stepper.setCurrentPosition(0);
+  stepper.moveTo(step);
+  while (stepper.currentPosition() != step)
+  {
+    if (step > 0)
+    {
+      stepper.setSpeed(m_stepper_speed_ppm);
+    }
+    else
+    {
+      stepper.setSpeed(-m_stepper_speed_ppm);
+    }
+    stepper.runSpeed();
+    uart_receive_and_execute();
+  }
+  stepper.stop();
+  Serial.println(stepper.currentPosition());
+}
+
+void m_stepper_set_speed(uint16_t speed_rpm)
+{
+  m_stepper_speed_rpm = speed_rpm;
+  m_stepper_speed_ppm = REVOLUTION_PER_MINUITE * speed_rpm;
 }
 
 /* End of file -------------------------------------------------------- */
